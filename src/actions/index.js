c15aaf63430e1616e2b5f901e944f73b2db2df9e
GMarguiller <gael.marguiller@supinternet.fr>
2020-07-21 20:05:49 +0200

    Start refactoring

---
+++ src/actions/index.js
@@ -0,0 +1,47 @@
+import {
+    SET_AUTHENTIFICATION, INCREMENT_ACTIONS_COUNT, ADD_RESSOURCE
+} from './action-types';
+import axios from "axios"
+const BASE_URL = "http://localhost:3090";
+
+export function setAuthentification(isLoggedIn) {
+    return {
+        type: SET_AUTHENTIFICATION,
+        payload: isLoggedIn
+    };
+}
+
+export function incrementActionCount() {
+    return {
+        type: INCREMENT_ACTIONS_COUNT,
+    };
+}
+
+export function addRessource() {
+
+    return {
+        type: ADD_RESSOURCE,
+    };
+}
+
+
+export function signinUser({ email, password }, history) {
+    return function (dispatch) {
+        axios.post(`${BASE_URL}/signin/`, { email, password })
+            .then((response) => {
+                dispatch({ type: SET_AUTHENTIFICATION, payload: true })
+                localStorage.setItem("token", response.data.token)
+                history.push("/ressources");
+            })
+            .catch((err) => {
+                console.log("erreur", err)
+            })
+    }
+}
+
+export function signinOutUser() {
+    return function (dispatch) {
+        dispatch({ type: SET_AUTHENTIFICATION, payload: false })
+        localStorage.removeItem("token")
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import store from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reducers from "./reducers";
+import thunk from "redux-thunk";
+import App from "./components/app";
+
+import { BrowserRouter } from "react-router-dom";
+import { actionLogger } from "../src/middlewares/action-logger"
+const invariant = require('redux-immutable-state-invariant').default()
+const createStoreWithMiddleware = applyMiddleware(thunk, actionLogger, invariant)(createStore);
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <Provider
+    store={createStoreWithMiddleware(
+      reducers,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__(),
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    )}
+  >
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.querySelector("#root")
+);
